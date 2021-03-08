e3acd959d5ca691b30b4c480a66172730ed310bb
Aruki <quanghuyn94@gmail.com>
2021-03-08 15:57:35 +0700

    Example

--- Application.py
+++ Application.py
@@ -4,20 +4,19 @@ import threading, time, wmi
 import vlc
 from tkinter import *
 	
-url = "Fubuki_Sirakami.mp4"
-hwndFrame = ""
+video = "Fubuki_Sirakami.mp4" #You video
 loop = False
 def Play(event):
 	global loop
 	if event.type == vlc.EventType.MediaPlayerEndReached:
 		loop = True
 
-def SimpleMediaPlayer(url):
+def SimpleMediaPlayer(video):
 	global loop
 	Instance = vlc.Instance()
 	player = Instance.media_player_new()
 	player.set_fullscreen(True)
-	media = Instance.media_new(url)
+	media = Instance.media_new(video)
 	player.set_media(media)
 	em = player.event_manager()
 	em.event_attach(vlc.EventType.MediaPlayerEndReached, Play)
@@ -31,5 +30,5 @@ def SimpleMediaPlayer(url):
 			player.play()
 			loop = False
 
-SimpleMediaPlayer(url)
+SimpleMediaPlayer(video)
 
