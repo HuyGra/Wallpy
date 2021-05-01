80e43662314ef67101c35a7fc76a99c98550fe79
Aruki <quanghuyn94@gmail.com>
2021-05-01 18:48:29 +0700

    Application.py

--- Application.py
+++ Application.py
@@ -1,30 +1,13 @@
-'''Here is an example. I hope you can do better.'''
-import WallpyLib
-import vlc
-	
-video = "Test~1.mp4" #You video
-loop = False
-def Play(event):
-	global loop
-	if event.type == vlc.EventType.MediaPlayerEndReached:
-		loop = True
+import WallpyLib, os, win32gui, tkinter, time
 
-def SimpleMediaPlayer(video):
-	global loop
-	Instance = vlc.Instance()
-	player = Instance.media_player_new()
-	player.set_fullscreen(True)
-	media = Instance.media_new(video)
-	player.set_media(media)
-	em = player.event_manager()
-	em.event_attach(vlc.EventType.MediaPlayerEndReached, Play)
-	player.play()
-	WallpyLib.Wallpy("VLC (Direct3D11 output)")
-	while True:
-		if loop:
-			player.set_media(media)
-			player.play()
-			loop = False
+os.startfile("WallpyVideoPlayer.exe")
 
-SimpleMediaPlayer(video)
-
+while True:
+    try:
+        f = open('cache/IntPtr.txt', 'r')
+        IntPtr = f.read()
+        f.close()
+        WallpyLib.Wallpy(IntPrt=int(IntPtr))
+        break
+    except:
+        pass
